98dac84783359bf6708a470d69a0e20073ba60f1
Dingsd4 <39777727+Dingsd4@users.noreply.github.com>
2018-12-12 19:19:09 +0100

    Added FtpServer

---
+++ Cave.Net/Ftp/FtpAccessType.cs
@@ -0,0 +1,35 @@
+﻿namespace Cave.Net.Ftp
+{
+    /// <summary>
+    /// Available ftp access types
+    /// </summary>
+    public enum FtpAccessType
+    {
+        /// <summary>Client wants to create a directory</summary>
+        CreateDirectory = 0x1001,
+
+        /// <summary>Client wants to change into a directory</summary>
+        ChangeDirectory,
+
+        /// <summary>Client wants to list the directory contents</summary>
+        ListDirectory,
+
+        /// <summary>Client wants to delete a directory</summary>
+        DeleteDirectory,
+
+        /// <summary>Client wants to upload a file</summary>
+        UploadFile = 0x2001,
+
+        /// <summary>Client wants to download a file</summary>
+        DownloadFile,
+
+        /// <summary>Client wants to delete a file</summary>
+        DeleteFile,
+
+        /// <summary>Client wants to rename a file or directory</summary>
+        RenameFrom = 0x3001,
+
+        /// <summary>Client wants to rename a file or directory</summary>
+        RenameTo = 0x3002,
+    }
+}
---
+++ Cave.Net/Ftp/FtpDirectoryEntryType.cs
@@ -0,0 +1,14 @@
+﻿namespace Cave.Net.Ftp
+{
+    /// <summary>
+    /// Provides all available FTP directory entry types
+    /// </summary>
+    public enum FtpDirectoryEntryType
+    {
+        /// <summary>The directory entry type</summary>
+        Directory = 'd',
+
+        /// <summary>The file entry type</summary>
+        File = '-',
+    }
+}
