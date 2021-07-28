806a30f00c10fc7348b7802e3869a77b337a1a48
Andreas Rohleder <andreas@rohleder.cc>
2021-07-28 21:51:43 +0200

    Added HttpConnectionTest

---
+++ Test/HttpConnectionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cave.Net;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    class HttpConnectionTest
+    {
+        [Test]
+        public void GetGoogle()
+        {
+            var google = HttpConnection.GetString("http://google.de");
+
+        }
+    }
+}
