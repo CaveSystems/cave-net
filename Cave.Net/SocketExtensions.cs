acf7d8d7a79740f3ce3a5495a25811ef653677e5
Andreas Rohleder <andreas@rohleder.cc>
2021-10-17 13:16:16 +0200

    Added SocketExtensions providing Socket.EnableDualSocket().

---
+++ Cave.Net/SocketExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Sockets;
+
+namespace Cave.Net
+{
+    /// <summary>
+    /// Provides extensions to the socket class.
+    /// </summary>
+    public static class SocketExtensions
+    {
+        /// <summary>
+        /// SocketOptionName for IPv6 only setting (not present in .Net 2.0)
+        /// </summary>
+        public const SocketOptionName Ipv6Only = (SocketOptionName)27;
+
+        /// <summary>
+        /// SocketOptionLevel for IPv6 (not present in .Net 2.0)
+        /// </summary>
+        public const SocketOptionLevel Ipv6 = (SocketOptionLevel)41;
+
+        /// <summary>
+        /// Enables dual socket on ipv4 and ipv6.
+        /// </summary>
+        /// <param name="socket"></param>
+        public static void EnableDualSocket(this Socket socket)
+        {
+            socket.SetSocketOption(Ipv6, Ipv6Only, false);
+        }
+    }
+}
