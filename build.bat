012de8200485289d34ebf381407b5906604b7048
Andreas Rohleder <andreas@rohleder.cc>
2021-11-27 15:31:54 +0100

    Updated build batch for test runs

--- build.bat
+++ build.bat
@@ -26,10 +26,13 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 rem msbuild /p:Configuration=Release /p:Platform="Any CPU" documentation.shfbproj
 rem if %errorlevel% neq 0 exit /b %errorlevel%
 
-Test\bin\Release\net20\Test.exe
-Test\bin\Release\net35\Test.exe
-Test\bin\Release\net40\Test.exe
-Test\bin\Release\net45\Test.exe
-Test\bin\Release\net46\Test.exe
-Test\bin\Release\net47\Test.exe
-Test\bin\Release\net48\Test.exe
+vstest.console Test\bin\Release\net5.0\Test.dll
+vstest.console Test\bin\Release\netcoreapp2.1\Test.dll
+vstest.console Test\bin\Release\netcoreapp3.1\Test.dll
+vstest.console Test\bin\Release\net20\Test.exe
+vstest.console Test\bin\Release\net35\Test.exe
+vstest.console Test\bin\Release\net40\Test.exe
+vstest.console Test\bin\Release\net45\Test.exe
+vstest.console Test\bin\Release\net46\Test.exe
+vstest.console Test\bin\Release\net47\Test.exe
+vstest.console Test\bin\Release\net48\Test.exe
